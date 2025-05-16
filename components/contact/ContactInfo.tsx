2cb6f8e826115ed3aa40b41e5b5303f0fb8e792b
Nykyta Koblianskyi <nkoblianskyi@gmail.com>
2025-05-16 14:18:08 +0200

    1

--- components/contact/ContactInfo.tsx
+++ components/contact/ContactInfo.tsx
@@ -66,20 +66,6 @@ export default function ContactInfo() {
         </div>
       </div>
 
-      <div className="mt-10">
-        <h3 className="font-semibold text-lg mb-4">Our Location</h3>
-        <div className="h-64 bg-gray-200 rounded-lg overflow-hidden">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3381.0968706849!2d34.77193491524158!3d32.0698089810193!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151d4b82a6148a07%3A0x9f5e6e5e6f8d57c0!2sAllenby%20St%2094%2C%20Tel%20Aviv-Yafo%2C%20Israel!5e0!3m2!1sen!2sus!4v1652345678901!5m2!1sen!2sus"
-            width="100%"
-            height="100%"
-            style={{ border: 0 }}
-            allowFullScreen={false}
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
-        </div>
-      </div>
     </div>
   )
 }
